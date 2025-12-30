c81a829c4e4fb325fb0746c563596f2fe1e71170
samyalaa10n <samyalaa638@gmail.com>
2025-12-31 00:31:54 +0200

    اوردر كل ساعة

--- src/app/Modules/Inventory/Category/Category.component.ts
+++ src/app/Modules/Inventory/Category/Category.component.ts
@@ -20,8 +20,8 @@ export class CategoryComponent implements OnInit {
     let Depart = await this._tools.Network.getAsync("Depart") as Array<any>;
     let Category = await this._tools.Network.getAsync("Category") as Array<any>;
     this.Columns.push(new Column('ID', "Code", "lapel", "text"))
-    this.Columns.push(new Column('NAME', "Name", "text", "text", 400))
-    this.Columns.push(new Column('FATHER', "Parent Category", "comboBox", "text", 400))
+    this.Columns.push(new Column('NAME', "Name", "text", "text", 200))
+    this.Columns.push(new Column('FATHER', "Parent Category", "comboBox", "text", 200))
     this.Columns[this.Columns.length - 1].columnComboBoxOptionLabel = "NAME";
     this.Columns[this.Columns.length - 1].columnComboBoxOptionValue = "ID";
     this.Columns[this.Columns.length - 1].columnComboBoxPlaceholder = "Parent Category"
@@ -31,32 +31,33 @@ export class CategoryComponent implements OnInit {
     this.Columns[this.Columns.length - 1].columnComboBoxOptionValue = "ID";
     this.Columns[this.Columns.length - 1].columnComboBoxPlaceholder = "Select the Department for this Category"
     this.Columns[this.Columns.length - 1].columnComboBoxDataSource = Depart;
+    this.Columns.push(new Column('REQUEST_MAX_LEMIT', "Maximum Available Quantity per Hour", "text", "text", 400))
   }
   config(dataGrid: DataGridComponent) {
     dataGrid.AllowCopyPest = true;
     dataGrid.Pest = async () => {
       var data = await navigator.clipboard.readText();
-      if (data && typeof data =="string") {
+      if (data && typeof data == "string") {
         let rows = data.split('\r');
-        let clondata=this._tools.cloneObject(dataGrid.dataSource);
+        let clondata = this._tools.cloneObject(dataGrid.dataSource);
         for (let i = 0; i < rows.length; i++) {
-          let Nitem={
-            ID:-1,
-            NAME:rows[i].replace('\n','').trim(),
-            ROW_NUMBER:-1,
-            FATHER:0,
-            DEPART:0
+          let Nitem = {
+            ID: -1,
+            NAME: rows[i].replace('\n', '').trim(),
+            ROW_NUMBER: -1,
+            FATHER: 0,
+            DEPART: 0
           }
           clondata.push(Nitem);
         }
-        dataGrid.dataSource=clondata;
+        dataGrid.dataSource = clondata;
       }
     }
   }
   async update() {
-      let Depart = await this._tools.Network.getAsync("Depart") as Array<any>;
-      let Category = await this._tools.Network.getAsync("Category") as Array<any>;
-      this.Columns[2].columnComboBoxDataSource = Category;
-      this.Columns[3].columnComboBoxDataSource = Depart;
-    }
+    let Depart = await this._tools.Network.getAsync("Depart") as Array<any>;
+    let Category = await this._tools.Network.getAsync("Category") as Array<any>;
+    this.Columns[2].columnComboBoxDataSource = Category;
+    this.Columns[3].columnComboBoxDataSource = Depart;
   }
+}
